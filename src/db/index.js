2cf166356ca2b21d44241b43b77370f9589d5f20
NoorMohdDev <noormohddev@gmail.com>
2025-06-22 19:34:31 +0530

    mongoAtlas connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
+import mongoose from "mongoose";
+
 
 const connectDB = async () => {
-  try {
-    const connectInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-    
-  } catch (error) {
-    console.log("MongoDB Connection Failed!", error);
-    process.exit(1);
-  }
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
 };
 
 export default connectDB;
