2047a026afaf78ec38cc5fd64337047bacc6195f
NoorMohdDev <noormohddev@gmail.com>
2025-06-22 18:55:22 +0530

    Database Connection

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import connectDB from "./db";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB().then(() => {
+    
+});
