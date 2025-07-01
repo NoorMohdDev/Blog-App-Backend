7b9fd617b9be698513f98935c6c82ab7d386e1a6
NoorMohdDev <noormohddev@gmail.com>
2025-07-01 21:21:37 +0530

    add user profile and account routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,14 @@ import {
   loginUser,
   logOutUser,
   registerUser,
-  refreshAccessToken
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserProfile,
+  getReadHistory,
 } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -28,6 +35,19 @@ router.route("/login").post(loginUser);
 
 //secured route
 router.route("/logout").post(verifyJWT, logOutUser);
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/update-cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+
+router.route("/b/:username").get(verifyJWT, getUserProfile);
+router.route("/history").get(verifyJWT, getReadHistory);
 
 export default router;
