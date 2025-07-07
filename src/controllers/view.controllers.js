72dbbfe8a5c290dd455e624a6965330f8ebdb929
NoorMohdDev <noormohddev@gmail.com>
2025-07-07 17:27:50 +0530

    feat: implement getPostViews controller and update routes for view tracking

--- src/controllers/view.controllers.js
+++ src/controllers/view.controllers.js
@@ -1,4 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import mongoose from "mongoose";
 import { isValidObjectId } from "mongoose";
 import { View } from "../models/view.model.js";
 import { ApiError } from "../utils/ApiError.js";
@@ -11,13 +12,11 @@ const addPostViews = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Invalid post ID");
   }
 
-  const userId = req.user?._id;
+  const user = await View.findOneAndUpdate({userId:req.user?._id, postId: postId});
 
-  const user = await View.findById(userId);
-  console.log(user);
   if (!user) {
     const newView = new View({
-      userId: userId,
+      userId: req.user?._id,
       postId: postId,
       viewCount: 1,
     });
@@ -28,14 +27,47 @@ const addPostViews = asyncHandler(async (req, res) => {
       .status(201)
       .json(new ApiResponse(200, newView, "View added successfully"));
   } else {
-    await updateView.save();
+    user.viewCount += 1;
+    await user.save({validateBeforeSave: false });
 
     return res
       .status(200)
-      .json(new ApiResponse(200, updateView, "View added successfully"));
+      .json(new ApiResponse(200, user, "View added successfully"));
   }
 });
 
-const getPostViews = asyncHandler(async (req, res) => {});
+const getPostViews = asyncHandler(async (req, res) => {
+  const { postId } = req.params;
+
+  if (!isValidObjectId(postId)) {
+    throw new ApiError(400, "Invalid post ID");
+  }
+
+  const views = await View.aggregate([{
+    $match:{
+      postId: new mongoose.Types.ObjectId(postId)
+    }
+  },{
+    $addFields:{
+      viewCount:"$viewCount"
+      
+    }
+  },
+  {
+    $group:{
+      _id: null,
+      viewCount: { $sum: "$viewCount" }
+    }
+  },{
+    $project:{
+      _id:0,
+      viewCount:1
+    }
+  }])
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, views[0].viewCount, "Views retrieved successfully"));
+});
 
 export { addPostViews, getPostViews };
--- src/routes/view.routes.js
+++ src/routes/view.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addPostViews } from "../controllers/view.controllers.js";
+import { addPostViews, getPostViews } from "../controllers/view.controllers.js";
 
 const router = Router();
 
 //routes
 
-router.route("/add/:postId").get(verifyJWT,addPostViews)
+router.route("/add/:postId").get(verifyJWT, addPostViews)
+router.route("/get-post-views/:postId").get(verifyJWT, getPostViews)
 
 export default router;
